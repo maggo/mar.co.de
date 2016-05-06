76ba5f2c715a2ad3b007d8b6f6eaffc812106a6d
Marco Kaul <marco.kaul@ueberbit.de>
2016-05-06 23:38:18 +0200

    Add simple file server

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-// Static file server goes here
+var http = require('http');
+var static = require('node-static');
+
+var fileServer = new static.Server('./app');
+
+http.createServer(function (request, response) {
+  request.addListener('end', function () {
+    fileServer.serve(request, response);
+  }).resume();
+}).listen(process.env.PORT || 3000);
