deabbc57a91f804f11da5190b6ed27c0e3f8028e
maggo <marcokaul@gmail.com>
2019-07-16 19:30:17 +0200

    Remove empty css rule

--- components/SocialMedias.tsx
+++ components/SocialMedias.tsx
@@ -4,7 +4,7 @@ import GithubIcon from "../icons/github.svg?sprite";
 
 export const SocialMedias = () => (
   <ul className={styles.container}>
-    <li className={styles.item}>
+    <li>
       <a
         href="https://twitter.com/mediaquery"
         title="I'm @mediaquery on twitter.com"
@@ -13,7 +13,7 @@ export const SocialMedias = () => (
         mediaquery
       </a>
     </li>
-    <li className={styles.item}>
+    <li>
       <a href="https://github.com/maggo" title="I'm @maggo on github.com">
         <GithubIcon role="img" aria-hidden="true" className={styles.icon} />
         maggo
