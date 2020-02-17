8da420412c92b35bb018682ed949354691363d52
maggo <marcokaul@gmail.com>
2020-02-17 14:21:05 +0100

    Update dependencies

--- next.config.js
+++ next.config.js
@@ -1,26 +1,9 @@
-const withCSS = require("@zeit/next-css");
 const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
 const withOffline = require("next-offline");
 
-module.exports = withPlugins(
-  [
-    [
-      withCSS,
-      {
-        cssModules: true,
-        cssLoaderOptions: {
-          importLoaders: 1,
-          localIdentName: "[name]__[local]--[hash:base64:5]"
-        }
-      }
-    ],
-    optimizedImages,
-    withOffline
-  ],
-  {
-    experimental: {
-      modern: true
-    }
+module.exports = withPlugins([optimizedImages, withOffline], {
+  experimental: {
+    modern: true
   }
-);
+});
